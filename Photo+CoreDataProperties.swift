37985f6c9419613ab5a0807ba35cff9949af8323
Zachary Eidenberger <zacharyg88@gmail.com>
2017-11-17 18:48:55 -0500

    Binary ImageData, prepareForReuse, and individual delete functioning

--- Photo+CoreDataProperties.swift
+++ Photo+CoreDataProperties.swift
@@ -2,8 +2,9 @@
 //  Photo+CoreDataProperties.swift
 //  VirtualTourist(CoreData)
 //
-//  Created by Zach Eidenberger on 10/28/17.
+//  Created by Zach Eidenberger on 11/17/17.
 //  Copyright © 2017 ZacharyG. All rights reserved.
+//
 //
 
 import Foundation
@@ -13,12 +14,13 @@ import CoreData
 extension Photo {
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Photo> {
-        return NSFetchRequest<Photo>(entityName: "Photo");
+        return NSFetchRequest<Photo>(entityName: "Photo")
     }
 
     @NSManaged public var id: Double
     @NSManaged public var imageURL: String?
     @NSManaged public var title: String?
+    @NSManaged public var imageData: NSData?
     @NSManaged public var pin: Pin?
 
 }
